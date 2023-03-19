6e5cf12507ed31d798842c5663de48cb0fb03039
ValentineRa <valentine.rault@live.fr>
2023-03-19 17:20:35 +0100

    modif class Videos

--- Twizzy_test/Twizzy_a_moi/src/Twizzy_a_moi/Main.java
+++ Twizzy_test/Twizzy_a_moi/src/Twizzy_a_moi/Main.java
@@ -5,19 +5,15 @@ import org.opencv.core.*;
 public class Main{
 	
 	public static void main(String[] args){
-		
+		/*
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		Init image=new Init("circles_rectangles.jpg");
 		DetectionImage.ImShow("hsv",image.gethsv());
 		DetectionImage.ImShow("pascontour",image.getimageread());
 		DetectionImage.ImShow("contour",image.getContours());
-		
-		try {
-			Videos.videos();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-			}
+		*/
+
+		new Videos("video1.avi" ); //changer place de la video
+	}
 
 }
